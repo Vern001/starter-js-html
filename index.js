8063c1dc62ac166d02ea294acbc6908fed4fe8d0
Amr Mousa <152765453+Vern001@users.noreply.github.com>
2025-06-30 03:09:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 alert('Welcome to the store!');
-// let user_stat=true;
-// let pass= 'hamo!@360';
-// let username= 'new atlantics';
-// let dihnamics_grade= 'B';
-// let hasnumber= /\d/;
+let user_stat=true;
+let pass= 'hamo!@360';
+let username= 'new atlantics';
+let dihnamics_grade= 'B';
+let hasnumber= /\d/;
 
-// if(!user_stat){
-//     console.log('user unavailable')
-// }else{
-//     console.log('user logged in')
-// }
-// if(pass==='hamo!@360'){
-
-
-//     if ((pass.length>=10&&pass.includes('$'))||(pass.includes('@')&&hasnumber.test(`${pass}`))){
-
-//         console.log('This is a good passsword!')
-
-//     }else if((pass.length>=4&&pass.length<=7)||(hasnumber.test(`${pass}`))){
-//         console.log('This is a decent password!')
-
-//     }else{
-//         console.log('This is a bad password!')
-//     }
-
-// }
+if(!user_stat){
+    console.log('user unavailable')
+}else{
+    console.log('user logged in')
+}
+if(pass==='hamo!@360'){
 
 
+    if ((pass.length>=10&&pass.includes('$'))||(pass.includes('@')&&hasnumber.test(`${pass}`))){
 
-// const biggernum=function(num1,num2){
-//     if(num1>num2){
-//         return num1
-//     }else{
-//         return num2
-//     }
+        console.log('This is a good passsword!')
 
-// };
+    }else if((pass.length>=4&&pass.length<=7)||(hasnumber.test(`${pass}`))){
+        console.log('This is a decent password!')
+
+    }else{
+        console.log('This is a bad password!')
+    }
+
+}
 
 
-// const maxsquare=function(num1,num2){
+
+const biggernum=function(num1,num2){
+    if(num1>num2){
+        return num1
+    }else{
+        return num2
+    }
+
+};
+
+
+const maxsquare=function(num1,num2){
     
-//     let x=biggernum(num1,num2);
-//     x=x**2
-//     return x;
-// };
+    let x=biggernum(num1,num2);
+    x=x**2
+    return x;
+};
 
-// let num1=5
-// let num2=10
+let num1=5
+let num2=10
 
-// let x = maxsquare(num1, num2);
-// console.log(x)
+let z = maxsquare(num1, num2);
+console.log(z)
 
 
-x=14
+let x=14
 
 const iseven= function(x){
 
